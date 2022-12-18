b7b191608332c1950c4050c663fffba0c31fe64e
Büşra KILINÇ <75065197+bussraklnc@users.noreply.github.com>
2022-12-18 23:40:06 +0300

    Add files via upload

---
+++ anasayfa3.hafta/Anasayfa.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace pansiyonotomasyonu
+{
+    partial class Anasayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnmaas = new System.Windows.Forms.Button();
+            this.btnstok = new System.Windows.Forms.Button();
+            this.btnmesaj = new System.Windows.Forms.Button();
+            this.btnradyo = new System.Windows.Forms.Button();
+            this.btnhakki = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnhava = new System.Windows.Forms.Button();
+            this.btngazete = new System.Windows.Forms.Button();
+            this.btncikis = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ADMİN GİRİŞ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(178, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 70);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "YENİ MÜŞTERİ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(311, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 70);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "ODALAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(46, 132);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 61);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "MÜŞTERİLER";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnmaas
+            // 
+            this.btnmaas.Location = new System.Drawing.Point(178, 132);
+            this.btnmaas.Name = "btnmaas";
+            this.btnmaas.Size = new System.Drawing.Size(127, 61);
+            this.btnmaas.TabIndex = 2;
+            this.btnmaas.Text = "GELİR-GİDER FORMU";
+            this.btnmaas.UseVisualStyleBackColor = true;
+            this.btnmaas.Click += new System.EventHandler(this.btnmaas_Click);
+            // 
+            // btnstok
+            // 
+            this.btnstok.Location = new System.Drawing.Point(311, 132);
+            this.btnstok.Name = "btnstok";
+            this.btnstok.Size = new System.Drawing.Size(109, 61);
+            this.btnstok.TabIndex = 3;
+            this.btnstok.Text = "STOKLAR";
+            this.btnstok.UseVisualStyleBackColor = true;
+            this.btnstok.Click += new System.EventHandler(this.btnstok_Click);
+            // 
+            // btnmesaj
+            // 
+            this.btnmesaj.Location = new System.Drawing.Point(46, 236);
+            this.btnmesaj.Name = "btnmesaj";
+            this.btnmesaj.Size = new System.Drawing.Size(126, 59);
+            this.btnmesaj.TabIndex = 4;
+            this.btnmesaj.Text = "MÜŞTERİ MESAJLARI";
+            this.btnmesaj.UseVisualStyleBackColor = true;
+            // 
+            // btnradyo
+            // 
+            this.btnradyo.Location = new System.Drawing.Point(178, 236);
+            this.btnradyo.Name = "btnradyo";
+            this.btnradyo.Size = new System.Drawing.Size(127, 59);
+            this.btnradyo.TabIndex = 5;
+            this.btnradyo.Text = "RADYO DİNLE";
+            this.btnradyo.UseVisualStyleBackColor = true;
+            this.btnradyo.Click += new System.EventHandler(this.btnradyo_Click);
+            // 
+            // btnhakki
+            // 
+            this.btnhakki.Location = new System.Drawing.Point(311, 236);
+            this.btnhakki.Name = "btnhakki";
+            this.btnhakki.Size = new System.Drawing.Size(109, 59);
+            this.btnhakki.TabIndex = 6;
+            this.btnhakki.Text = "HAKKIMIZDA";
+            this.btnhakki.UseVisualStyleBackColor = true;
+            this.btnhakki.Click += new System.EventHandler(this.btnhakki_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(665, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(731, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnhava
+            // 
+            this.btnhava.Location = new System.Drawing.Point(442, 32);
+            this.btnhava.Name = "btnhava";
+            this.btnhava.Size = new System.Drawing.Size(110, 70);
+            this.btnhava.TabIndex = 9;
+            this.btnhava.Text = "ŞİFRE GÜNCELLEME";
+            this.btnhava.UseVisualStyleBackColor = true;
+            this.btnhava.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btngazete
+            // 
+            this.btngazete.Location = new System.Drawing.Point(442, 132);
+            this.btngazete.Name = "btngazete";
+            this.btngazete.Size = new System.Drawing.Size(110, 61);
+            this.btngazete.TabIndex = 10;
+            this.btngazete.Text = "HABERLER";
+            this.btngazete.UseVisualStyleBackColor = true;
+            this.btngazete.Click += new System.EventHandler(this.btngazete_Click);
+            // 
+            // btncikis
+            // 
+            this.btncikis.Location = new System.Drawing.Point(442, 236);
+            this.btncikis.Name = "btncikis";
+            this.btncikis.Size = new System.Drawing.Size(110, 59);
+            this.btncikis.TabIndex = 11;
+            this.btncikis.Text = "ÇIKIŞ";
+            this.btncikis.UseVisualStyleBackColor = true;
+            this.btncikis.Click += new System.EventHandler(this.btncikis_Click);
+            // 
+            // Anasayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btncikis);
+            this.Controls.Add(this.btngazete);
+            this.Controls.Add(this.btnhava);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnhakki);
+            this.Controls.Add(this.btnradyo);
+            this.Controls.Add(this.btnmesaj);
+            this.Controls.Add(this.btnstok);
+            this.Controls.Add(this.btnmaas);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Anasayfa";
+            this.Text = "Anasayfa";
+            this.Load += new System.EventHandler(this.Anasayfa_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnmaas;
+        private System.Windows.Forms.Button btnstok;
+        private System.Windows.Forms.Button btnmesaj;
+        private System.Windows.Forms.Button btnradyo;
+        private System.Windows.Forms.Button btnhakki;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnhava;
+        private System.Windows.Forms.Button btngazete;
+        private System.Windows.Forms.Button btncikis;
+    }
+}
---
+++ anasayfa3.hafta/Anasayfa.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace pansiyonotomasyonu
+{
+    public partial class Anasayfa : Form
+    {
+        public Anasayfa()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GirisSayfasi fr = new GirisSayfasi();
+            fr.Show();
+            this.Hide();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            YeniMüşteri fr = new YeniMüşteri();
+            fr.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Odalar fr = new Odalar();
+            fr.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Musteriler fr = new Musteriler();
+            fr.Show();
+        }
+
+
+        //------------2.HAFTA-------------
+        private void btnstok_Click(object sender, EventArgs e)
+        {
+            Stoklar fr = new Stoklar();
+            fr.Show();
+        }
+
+        //--------------3.HAFTA------------
+        private void btnradyo_Click(object sender, EventArgs e)
+        {
+            Radyo fr = new Radyo();
+            fr.Show();
+        }
+
+        private void Anasayfa_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+
+        //-------------2.HAFTA----------------------
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongDateString();
+            label2.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        //---------3.Hafta----------------
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SifreGuncelleme fr = new SifreGuncelleme();
+            fr.Show();
+        }
+
+        //--------------2.HAFTA------------------------
+        private void btncikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void btnmaas_Click(object sender, EventArgs e)
+        {
+            GelirGider fr = new GelirGider();
+            fr.Show();
+
+        }
+
+        //------------3.HAFTA-------------------------------
+        private void btngazete_Click(object sender, EventArgs e)
+        {
+            Haberler fr = new Haberler();
+            fr.Show();
+        }
+
+        private void btnhakki_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ADEM PANSİYON 2022'DE KURULMUŞTUR");
+        }
+    }
+}
