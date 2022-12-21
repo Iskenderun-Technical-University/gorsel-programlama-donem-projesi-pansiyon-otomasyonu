1b92cce3e75cada877d3b2093c72953345ae7799
selcukbatihan <117325894+selcukbatihan@users.noreply.github.com>
2022-12-21 21:45:55 +0300

    Add files via upload

---
+++ stoklarTAMAMLANDI/Stoklar.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace pansiyonotomasyonu
+{
+    partial class Stoklar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtgida = new System.Windows.Forms.TextBox();
+            this.txticecek = new System.Windows.Forms.TextBox();
+            this.txtatistir = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnstokkaydet = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtelektrik = new System.Windows.Forms.TextBox();
+            this.txtsu = new System.Windows.Forms.TextBox();
+            this.txtint = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnkydt2 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(48, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GIDA TUTAR:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(48, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İÇECEK TUTARI:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(48, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ATIŞTIRMALIKLAR:";
+            // 
+            // txtgida
+            // 
+            this.txtgida.Location = new System.Drawing.Point(171, 15);
+            this.txtgida.Name = "txtgida";
+            this.txtgida.Size = new System.Drawing.Size(88, 20);
+            this.txtgida.TabIndex = 3;
+            // 
+            // txticecek
+            // 
+            this.txticecek.Location = new System.Drawing.Point(171, 62);
+            this.txticecek.Name = "txticecek";
+            this.txticecek.Size = new System.Drawing.Size(88, 20);
+            this.txticecek.TabIndex = 4;
+            // 
+            // txtatistir
+            // 
+            this.txtatistir.Location = new System.Drawing.Point(171, 112);
+            this.txtatistir.Name = "txtatistir";
+            this.txtatistir.Size = new System.Drawing.Size(88, 20);
+            this.txtatistir.TabIndex = 5;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(6, 234);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(332, 97);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Gida";
+            this.columnHeader1.Width = 89;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "İcecek";
+            this.columnHeader4.Width = 111;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Atistirmalik";
+            this.columnHeader5.Width = 127;
+            // 
+            // btnstokkaydet
+            // 
+            this.btnstokkaydet.BackColor = System.Drawing.Color.LightGray;
+            this.btnstokkaydet.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.btnstokkaydet.Location = new System.Drawing.Point(171, 168);
+            this.btnstokkaydet.Name = "btnstokkaydet";
+            this.btnstokkaydet.Size = new System.Drawing.Size(88, 19);
+            this.btnstokkaydet.TabIndex = 7;
+            this.btnstokkaydet.Text = "KAYDET";
+            this.btnstokkaydet.UseVisualStyleBackColor = false;
+            this.btnstokkaydet.Click += new System.EventHandler(this.btnstokkaydet_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(23, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "ELEKTRİK:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(23, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "SU:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(23, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "İNTERNET:";
+            // 
+            // txtelektrik
+            // 
+            this.txtelektrik.Location = new System.Drawing.Point(132, 16);
+            this.txtelektrik.Name = "txtelektrik";
+            this.txtelektrik.Size = new System.Drawing.Size(100, 20);
+            this.txtelektrik.TabIndex = 3;
+            // 
+            // txtsu
+            // 
+            this.txtsu.Location = new System.Drawing.Point(132, 63);
+            this.txtsu.Name = "txtsu";
+            this.txtsu.Size = new System.Drawing.Size(100, 20);
+            this.txtsu.TabIndex = 4;
+            // 
+            // txtint
+            // 
+            this.txtint.Location = new System.Drawing.Point(132, 113);
+            this.txtint.Name = "txtint";
+            this.txtint.Size = new System.Drawing.Size(100, 20);
+            this.txtint.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.txtgida);
+            this.groupBox1.Controls.Add(this.txticecek);
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtatistir);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnstokkaydet);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(344, 335);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "MUTFAK";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.btnkydt2);
+            this.groupBox2.Controls.Add(this.listView2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtint);
+            this.groupBox2.Controls.Add(this.txtelektrik);
+            this.groupBox2.Controls.Add(this.txtsu);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox2.Location = new System.Drawing.Point(362, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(352, 335);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "FATURALAR";
+            // 
+            // btnkydt2
+            // 
+            this.btnkydt2.BackColor = System.Drawing.Color.LightGray;
+            this.btnkydt2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.btnkydt2.Location = new System.Drawing.Point(132, 164);
+            this.btnkydt2.Name = "btnkydt2";
+            this.btnkydt2.Size = new System.Drawing.Size(100, 23);
+            this.btnkydt2.TabIndex = 9;
+            this.btnkydt2.Text = "KAYDET";
+            this.btnkydt2.UseVisualStyleBackColor = false;
+            this.btnkydt2.Click += new System.EventHandler(this.btnkydt2_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader6});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(0, 234);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(352, 97);
+            this.listView2.TabIndex = 8;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Elektrik";
+            this.columnHeader2.Width = 81;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Su";
+            this.columnHeader3.Width = 71;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "İnternet";
+            this.columnHeader6.Width = 141;
+            // 
+            // Stoklar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::pansiyonotomasyonu.Properties.Resources.WhatsApp_Image_2022_12_21_at_10_25_24;
+            this.ClientSize = new System.Drawing.Size(720, 349);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Stoklar";
+            this.Text = "Stoklar";
+            this.Load += new System.EventHandler(this.Stoklar_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtgida;
+        private System.Windows.Forms.TextBox txticecek;
+        private System.Windows.Forms.TextBox txtatistir;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnstokkaydet;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtelektrik;
+        private System.Windows.Forms.TextBox txtsu;
+        private System.Windows.Forms.TextBox txtint;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Button btnkydt2;
+    }
+}
---
+++ stoklarTAMAMLANDI/Stoklar.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace pansiyonotomasyonu
+{
+    public partial class Stoklar : Form
+    {
+        public Stoklar()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=AdemPansiyonn;Integrated Security=True");
+
+
+        //--------------2.HAFTA--------------------
+        private void veriler()
+        {
+
+            listView1.Items.Clear();
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select * from Stoklar", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["Gida"].ToString();
+                ekle.SubItems.Add(oku["İcecek"].ToString());
+                ekle.SubItems.Add(oku["Cerezler"].ToString());
+                listView1.Items.Add(ekle);
+            }
+            baglanti.Close();
+
+        }
+
+        //---------------3.HAFTA-----------------
+        private void veriler2()
+        {
+            listView2.Items.Clear();
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("Select * from Fatura", baglanti);
+            SqlDataReader oku2 = komut2.ExecuteReader();
+
+            while (oku2.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku2["Elektrik"].ToString();
+                ekle.SubItems.Add(oku2["Su"].ToString());
+                ekle.SubItems.Add(oku2["Internet"].ToString());
+                listView2.Items.Add(ekle);
+            }
+            baglanti.Close();
+
+        }
+
+        private void Stoklar_Load(object sender, EventArgs e)
+        {
+            veriler();
+            veriler2();
+        }
+
+        private void btnstokkaydet_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into Stoklar (Gida,İcecek,Cerezler) VALUES ( @gida , @icecek , @cerez  )", baglanti);
+            komut.Parameters.AddWithValue("@gida", txtgida.Text);
+            komut.Parameters.AddWithValue("@icecek", txticecek.Text);
+            komut.Parameters.AddWithValue("@cerez", txtatistir.Text);
+
+
+
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+
+        //-------------4.hafta----------------
+
+        private void btnkydt2_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("insert into Fatura (Elektrik,Su,Internet) VALUES ( @elektrik , @su , @internet  )", baglanti);
+            komut2.Parameters.AddWithValue("@elektrik", txtelektrik.Text);
+            komut2.Parameters.AddWithValue("@su", txtsu.Text);
+            komut2.Parameters.AddWithValue("@internet", txtint.Text);
+
+
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+        }
+    }
+}
