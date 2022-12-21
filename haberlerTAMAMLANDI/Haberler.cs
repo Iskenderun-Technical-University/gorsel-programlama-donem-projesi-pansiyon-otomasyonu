e29a4f93f19d6f9704912bab6ce08959b40c5668
Emine Karaşoğlu <117325174+eminekarasoglu@users.noreply.github.com>
2022-12-21 21:49:35 +0300

    Add files via upload

---
+++ haberlerTAMAMLANDI/Haberler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pansiyonotomasyonu
+{
+    public partial class Haberler : Form
+    {
+        public Haberler()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.sozcu.com.tr/");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.haberturk.com/");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.sporx.com/");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://onedio.com/");
+        }
+
+        private void Haberler_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
